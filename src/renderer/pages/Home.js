57d7b8664f2a46779abfc51dca983717ac12bbfd
Váci Kornél <vacikorni@gmail.com>
2019-02-11 23:02:45 +0100

    Youtube link filter

--- src/renderer/pages/Home.js
+++ src/renderer/pages/Home.js
@@ -5,6 +5,7 @@ export default class Home extends Component {
   constructor(props) {
     super(props);
     this.updateLinks = this.updateLinks.bind(this);
+    this.deleteLink = this.deleteLink.bind(this);
     this.state = {
       links: []
     }
@@ -16,19 +17,35 @@ export default class Home extends Component {
     })
   }
 
+  deleteLink(key) {
+    var arr = [...this.state.links];
+    arr.splice(key, 1);
+    this.setState({
+      links: [...arr]
+    })
+  }
+
   componentDidMount() {
     clipboard.on('text-changed', () => {
-      this.updateLinks(clipboard.readText());
+      var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+      var text = clipboard.readText()
+      var link = text.match(regExp);
+      if (link != null)
+        this.updateLinks(text);
     }).startWatching();
+  }
+
+  componentWillUnmount() {
+    clipboard.stopWatching();
   }
 
   render() {
     var { links } = this.state
-    console.log(links);
+    //console.log(links);
     return (
       <div>
         {links.map((link, index) => {
-          return <div key={index}>{link}</div>
+          return <div onClick={() => this.deleteLink(index)} key={index}>{link}</div>
         })}
       </div>
     )
